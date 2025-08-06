14200cc075eb6ec106f4e36642b59e89fc65fbf1
Paul Ciurean <paul.ciurean@rebura.com>
2025-08-06 12:24:44 +0100

    4nd test

--- main.tf
+++ main.tf
@@ -2,42 +2,42 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = var.bucket_name
+# resource "aws_s3_bucket" "app_bucket" {
+#   bucket = var.bucket_name
 
-  tags = {
-    Name        = "MyAppBucket"
-    Environment = "Dev"
-    CreatedBy   = "Terraform"
-  }
-}
+#   tags = {
+#     Name        = "MyAppBucket"
+#     Environment = "Dev"
+#     CreatedBy   = "Terraform"
+#   }
+# }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "lambda-exec-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+# resource "aws_iam_role" "lambda_exec" {
+#   name = "lambda-exec-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "lambda.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
+# resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+#   role       = aws_iam_role.lambda_exec.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# }
 
-resource "aws_lambda_function" "my_lambda" {
-  filename         = "lambda_function_payload.zip"
-  function_name    = "MyLambdaFunction"
-  role             = aws_iam_role.lambda_exec.arn
-  handler          = "index.handler"
-  runtime          = "nodejs18.x"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
-}
+# resource "aws_lambda_function" "my_lambda" {
+#   filename         = "lambda_function_payload.zip"
+#   function_name    = "MyLambdaFunction"
+#   role             = aws_iam_role.lambda_exec.arn
+#   handler          = "index.handler"
+#   runtime          = "nodejs18.x"
+#   source_code_hash = filebase64sha256("lambda_function_payload.zip")
+# }
