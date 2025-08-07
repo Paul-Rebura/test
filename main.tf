ac0b7924f77ac9844376726b2c4fc694881d12d0
Paul-Rebura <paul.ciurean@rebura.com>
2025-08-07 16:06:14 +0100

    first commit

--- main.tf
+++ main.tf
@@ -1,43 +1,16 @@
 provider "aws" {
-  region = var.aws_region
+  region = "eu-west-2"
 }
 
-# resource "aws_s3_bucket" "app_bucket" {
-#   bucket = var.bucket_name
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
 
-#   tags = {
-#     Name        = "MyAppBucket"
-#     Environment = "Dev"
-#     CreatedBy   = "Terraform"
-#   }
-# }
-
-# resource "aws_iam_role" "lambda_exec" {
-#   name = "lambda-exec-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole",
-#         Effect = "Allow",
-#         Principal = {
-#           Service = "lambda.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
-
-# resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-#   role       = aws_iam_role.lambda_exec.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
-
-# resource "aws_lambda_function" "my_lambda" {
-#   filename         = "lambda_function_payload.zip"
-#   function_name    = "MyLambdaFunction"
-#   role             = aws_iam_role.lambda_exec.arn
-#   handler          = "index.handler"
-#   runtime          = "nodejs18.x"
-#   source_code_hash = filebase64sha256("lambda_function_payload.zip")
-# }
+  resource "aws_s3_bucket" "name" {
+    bucket = "my-unique-bucket-terraform-demo-07-08"
+  }
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" {
-  default = "eu-west-2"
-}
-
-variable "bucket_name" {
-  default = "my-unique-bucket-terraform-demo-123456"
-}
