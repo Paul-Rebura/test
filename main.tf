dd8a4aa162841c2e71c2b6dd6e91569d467c18f5
Paul-Rebura <paul.ciurean@rebura.com>
2025-08-12 14:15:52 +0100

    added vpc and subnet

--- main.tf
+++ main.tf
@@ -22,5 +22,23 @@ resource "aws_s3_bucket" "name" {
   }
 }
 
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name        = "My VPC for Terraform Demo"
+    Environment = "Dev"
+    Owner       = "Paul"
+    Anotherowner    = "Ciprian"
+  }
+}
 
-# terraform init -upgrade -backend-config="bucket=my-backend-bucket-for-statefile" -backend-config="key=codepipeline/pipeline.tfstate" -backend-config="region=eu-west-2" -backend-config="dynamodb_table=terraform-locks" -backend-config="encrypt=true"
+resource "aws_subnet" "first" {
+  vpc_id            = aws_vpc.name.id
+  cidr_block        = "10.0.5.0.24"
+  availability_zone = "eu-west-2a"
+  tags = {
+    Name        = "My First Subnet for Terraform Demo"
+    Environment = "Dev"
+    Owner       = "Paul"
+    Anotherowner    = "Ciprian"
+}
